77b9885160c3d5a51b97a74452484d3fa75aee35
Maycon Spirlandelli <mayconbatista8@gmail.com>
2024-12-24 07:22:54 -0300

    teste 03

--- player.py
+++ player.py
@@ -1,11 +1,12 @@
-#from streamlit_player import st_player
+from streamlit_player import st_player
 import streamlit as st
 
 # Embed a youtube video
-#st_player("https://youtu.be/CmSKVW1v0xM")
+st_player("https://youtu.be/CmSKVW1v0xM")
 
 # Embed a music from SoundCloud
-#st_player("https://soundcloud.com/imaginedragons/demons")
+st_player("https://soundcloud.com/imaginedragons/demons")
 
 
 st.title('Uber pickups in NYC')
+st.title("novo teste"")
